6ae223df40e38edfb8bfde3521e3f06f8d134a82
StalckGx <vinny.polsom@outlook.com>
2019-11-05 00:04:18 -0300

    Animation Added

--- script.js
+++ script.js
@@ -31,4 +31,38 @@ document.addEventListener ("DOMContentLoaded", function () {
         div.tempColor = "#" + Math.random().toString(16).substr(-6);
         div.style.backgroundColor = div.tempColor;
     });
-});
+
+    div.steps = Math.random() * 20;
+    div.direction = Math.floor (Math.random() * 4);
+    window.requestAnimationFrame(move);
+})
+
+function move () {
+    let speed = Math.random() * 10 + 15;
+    const box = document.querySelector (".box");
+    let bounds = output.getBoundingClientRect();
+    
+    box.steps--;
+    if (box.steps < 0) {
+        box.direction = Math.floor(Math.random() * 4);
+        box.steps = Math.random() * 20;
+    }
+
+    if (box.direction == 0 && box.x < bounds.right - 100) {
+        box.x += speed;
+    }
+    if (box.direction == 1 && box.x > bounds.left) {
+        box.x -= speed;
+    }
+    if (box.direction == 2 && box.y < bounds.bottom - 100) {
+        box.y += speed;
+    }
+    if (box.direction == 3 && box.y > bounds.top) {
+        box.y -= speed;
+    }
+
+    box.style.top = box.y + "px";
+    box.style.left = box.x + "px";
+
+    window.requestAnimationFrame(move);
+}
