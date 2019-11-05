060a6c412ad13bd5e608b9bfeb8d6791abc60d7a
StalckGx <vinny.polsom@outlook.com>
2019-11-04 23:31:58 -0300

    Target Element Created

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const output = document.querySelector(".output");
 const messageOut = document.querySelectorAll(".message span");
+
 output.addEventListener("mouseenter", function () {
     output.style.backgroundColor = "cyan";
 });
@@ -9,4 +10,14 @@ output.addEventListener("mouseleave", function () {
 output.addEventListener("mousemove", function (e) {
     messageOut[0].innerText = e.x
     messageOut[1].innerText = e.y
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let div  = document.createElement("div");
+    div.classList.add("box");
+    output.appendChild(div);
+    div.x = div.offsetLeft;
+    div.y = div.offsetTop;
+    div.tempColor = "#" + Math.random().toString(16).substr(-6);
+    div.style.backgroundColor = div.tempColor;
 });
